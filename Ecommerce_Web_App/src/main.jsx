02376f9f963fe054e487c5683cabaa57796f8f3a
AnishMishra1 <anishmishra143@gmail.com>
2023-11-30 18:10:28 +0530

    Adding Redux store and authslice

--- Ecommerce_Web_App/src/main.jsx
+++ Ecommerce_Web_App/src/main.jsx
@@ -4,12 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import store from './Redux/Store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <App />
-  <Toaster />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+         <App />
+         <Toaster />
+   </BrowserRouter>
+
+  </Provider>
+ 
     
   
 )
